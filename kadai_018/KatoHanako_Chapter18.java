740f1099cc5e4b7d6db445b650ca2585f4e6dbc6
Suama111333 <suama.the.suama@gmail.com>
2025-02-02 18:28:45 +0900

    Add files via upload

---
+++ kadai_018/KatoHanako_Chapter18.java
@@ -0,0 +1,12 @@
+package kadai_018;
+
+public class KatoHanako_Chapter extends Kato_Chapter18 {
+
+    public KatoHanako_Chapter() {
+        setGivenName("花子");
+    }
+
+    public void eachIntroduce() {
+        System.out.println("趣味は読書です");
+    }
+}
