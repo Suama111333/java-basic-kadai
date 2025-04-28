911bb645636c2976d6e9a475ae51b1a2ec6d1e51
Suama111333 <suama.the.suama@gmail.com>
2025-04-28 20:58:28 +0900

    Update KatoIchiro_Chapter18.java

--- kadai_018/KatoIchiro_Chapter18.java
+++ kadai_018/KatoIchiro_Chapter18.java
@@ -1,8 +1,9 @@
 package kadai_018;
 
 public class KatoIchiro_Chapter18 extends Kato_Chapter18 {
-    public KatoIchiro_Chapter18() {
-        super("加藤", "一郎", "東京都中野区◯×");
+ 
+    public void setGivenName(){
+        this.givenName = "一郎";
     }
 
     @Override
