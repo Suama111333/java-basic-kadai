ee7f23445ef8e3245226ca304de9c3cf956625f1
Suama111333 <suama.the.suama@gmail.com>
2025-04-28 21:30:57 +0900

    Update KatoExec_Chapter18.java

--- kadai_018/KatoExec_Chapter18.java
+++ kadai_018/KatoExec_Chapter18.java
@@ -1,26 +1,16 @@
 package kadai_018;
 
-public abstract class Kato_Chapter18 {
-	
-	public String familyName = "加藤";
-	public String givenName;
-	public String address    = "東京都中野区⚪×";
-	
-	public void setGivenName(String givenName) {
-        this.givenName = givenName;
-	}
-	
-	public void commonIntroduce() {
-		System.out.println("名前は" + familyName + givenName + "です");
-		System.out.println("住所は" + address + "です");
-	}
-	
-	public abstract void eachIntroduce();
-	
-	public void execIntroduce() {
-		commonIntroduce();
-		eachIntroduce();
-	}
+public class KatoExec_Chapter18 {
 
+    public static void main(String[] args) {
+        // 各インスタンス作成
+        KatoTaro_Chapter18 katoTaro = new KatoTaro_Chapter18();
+        KatoIchiro_Chapter18 katoIchiro = new KatoIchiro_Chapter18();
+        KatoHanako_Chapter18 katoHanako = new KatoHanako_Chapter18();
+
+        // execIntroduceメソッドを呼び出すことで、共通の紹介とそれぞれの紹介を表示
+        katoTaro.execIntroduce();
+        katoIchiro.execIntroduce();
+        katoHanako.execIntroduce();
+    }
 }
-
