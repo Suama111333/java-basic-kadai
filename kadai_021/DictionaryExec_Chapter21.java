b7f62cab642f18feee950788e92997fabf0fa419
Suama111333 <suama.the.suama@gmail.com>
2025-05-06 17:13:31 +0900

    Update DictionaryExec_Chapter21.java

--- kadai_021/DictionaryExec_Chapter21.java
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -1,17 +1,14 @@
 package kadai_021;
 
 public class DictionaryExec_Chapter21 {
-	
+
 	 public static void main(String[] args) {
-		 Dictionary_Chapter21 Dictionary = new Dictionary_Chapter21();
-		 
-		 Dictionary.wordPut();
-		 
+		 Dictionary_Chapter21 dictionary = new Dictionary_Chapter21();
+
 		 String[] wordArray = {"apple" , "banana" , "grape" , "orange"};
-		 
+
 		 for(String word : wordArray) {
-			 Dictionary.setSearchWord(word);
-			 Dictionary.search();
+			 dictionary.wordPut(word);
 		 }
 	 }
 
