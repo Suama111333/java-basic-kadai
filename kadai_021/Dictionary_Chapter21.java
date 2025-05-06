160450b3b0e83d562eac1a3f26b682fc85669272
Suama111333 <suama.the.suama@gmail.com>
2025-05-06 17:14:05 +0900

    Update Dictionary_Chapter21.java

--- kadai_021/Dictionary_Chapter21.java
+++ kadai_021/Dictionary_Chapter21.java
@@ -1,34 +1,28 @@
 package kadai_021;
-
 import java.util.HashMap;
 
-public class DictionaryChapter21 extends DictionaryExecChapter21 {
-    
-    HashMap<String, String> dictionary = new HashMap<String, String>();
-    String word;
-    
-    public void wordPut() {
+public class Dictionary_Chapter21 {
+    HashMap<String,String> dictionary = new HashMap<String,String>();
+
+    public Dictionary_Chapter21() {
         dictionary.put("apple", "りんご");
         dictionary.put("peach", "桃");
         dictionary.put("banana", "バナナ");
         dictionary.put("lemon", "レモン");
         dictionary.put("pear", "梨");
-        dictionary.put("kiwi", "キウイ");
+        dictionary.put("kiwi", "キウィ");
         dictionary.put("strawberry", "いちご");
         dictionary.put("grape", "ぶどう");
         dictionary.put("muscat", "マスカット");
         dictionary.put("cherry", "さくらんぼ");
-     }
-    
-     public void setSearchWord(String word) {
-         this.word = word;
-     } 
+    }
 
-     public void search() {
-         if (dictionary.containsKey(word)) {
-             System.out.println(word + "は" + dictionary.get(word) + "です");
-         } else {
-             System.out.println(word + "は辞書に存在しません");
-         }
-     }
+    // 英単語を取得するメソッド
+    public String wordPut(String word) {
+        if (dictionary.containsKey(word)) {
+            return word + "の意味は" + dictionary.get(word) + "です";
+        } else {
+            return word + "は辞書に存在しません";
+        }
+    }
 }
